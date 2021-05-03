339a68742cb00f515c0e2f13d8b1dbf5be68d3cf
Bartosz Dudek <50589551+bdudek1@users.noreply.github.com>
2021-05-03 15:04:58 +0200

    added skeletons

--- src/containers/TrainMe/ImageRow/ImageRow.js
+++ src/containers/TrainMe/ImageRow/ImageRow.js
@@ -6,15 +6,18 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
+import Skeleton from '@material-ui/lab/Skeleton';
 import CloseIcon from '@material-ui/icons/Close';
 
 
 const ImageRow = (props) => {
-    const predictionText = `The image is in ${parseFloat(props.file.viewPrediction*100).toFixed(2)}% a view`
+    const predictionText = (props.file.viewPrediction || props.file.viewPrediction === 0)? 
+    `The image is in ${parseFloat(props.file.viewPrediction*100).toFixed(2)}% a view` : null;
+
     const highestPredictionText = props.file.highestPrediction ? 
     `The image is ${props.file.highestPrediction.name} in ${parseFloat(props.file.highestPrediction.value*100).toFixed(2)}%` : null
 
-    let imgNameDisplayText = props.file.name
+    let imgNameDisplayText = props.file.name ? props.file.name : null
     let imgNameLength = props.file.name.length;
 
     if(imgNameLength > 12){
@@ -25,22 +28,22 @@ const ImageRow = (props) => {
             <Card className="ImageRow" style={{position:"relative"}}>
                 <CardContent className="CardContent">
                     <div className="CardDetails">
-                        <CardMedia
+                        {props.file.image ? <CardMedia
                             component="img"
                             image={props.file.image ? props.file.image.src : null}
                             title={imgNameDisplayText}
-                            className="CardImg"/>
+                            className="CardImg"/> : <Skeleton variant="rect" width={80} height={60} />}
                         <Typography component="h7" variant="h7">
-                            {imgNameDisplayText}
+                            {imgNameDisplayText ? imgNameDisplayText : <Skeleton variant="text" width={80} height={20} />}
                         </Typography>
                     </div>
 
                         <div className="CardDetails">
                             <Typography component="h7" variant="h7" className="CardText">
-                                {predictionText}
+                                {predictionText ? predictionText : <Skeleton variant="text" width={150} height={20}/>}
                             </Typography>
                             <Typography component="h7" variant="h7" color="textSecondary" style={{marginTop: "5px"}}>
-                                {highestPredictionText}
+                                {highestPredictionText ? highestPredictionText : <Skeleton variant="text" width={150} height={20}/>}
                             </Typography>
                         </div>
                         <div className="CardDetails">
